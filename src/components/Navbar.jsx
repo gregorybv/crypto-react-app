d752eebaa75bb0bab4ea258f90c9651f91c3d135
Gregory <gregory_berezin@outlook.com>
2023-05-06 10:44:45 +0300

    new edits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import ThemeToggle from './ThemeToggle';
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
-import { UserAuth } from '../context/AuthContext';
+import React, { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import ThemeToggle from "./ThemeToggle"
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai"
+import { UserAuth } from "../context/AuthContext"
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
-  const { user, logout } = UserAuth();
-  const navigate = useNavigate();
+  const [nav, setNav] = useState(false)
+  const { user, logout } = UserAuth()
+  const navigate = useNavigate()
 
   const handleNav = () => {
-    setNav(!nav);
-  };
+    setNav(!nav)
+  }
 
   const handleSignOut = async () => {
     try {
-      await logout();
-      navigate('/');
+      await logout()
+      navigate("/")
     } catch (e) {
-      console.log(e.message);
+      console.log(e.message)
     }
-  };
+  }
 
   return (
     <div className='rounded-div flex items-center justify-between h-20 font-bold'>
@@ -61,19 +61,19 @@ const Navbar = () => {
       <div
         className={
           nav
-            ? 'md:hidden fixed left-0 top-20 flex flex-col items-center justify-between w-full h-[90%] bg-primary ease-in duration-300 z-10'
-            : 'fixed left-[-100%] top-20 h-[90%] flex flex-col items-center justify-between ease-in duration-300'
+            ? "md:hidden fixed left-0 top-20 flex flex-col items-center justify-between w-full h-[90%] bg-primary ease-in duration-200 z-10"
+            : "fixed left-[-100%] top-20 h-[90%] flex flex-col items-center justify-between ease-in duration-200"
         }
       >
-        <ul className='w-full p-4'>
+        <ul className='w-full p-4 relative'>
+          <li className='absolute right-0'>
+            <ThemeToggle />
+          </li>
           <li onClick={handleNav} className='border-b py-6'>
             <Link to='/'>Home</Link>
           </li>
           <li onClick={handleNav} className='border-b py-6'>
             <Link to='/account'>Account</Link>
-          </li>
-          <li className=' py-6'>
-            <ThemeToggle />
           </li>
         </ul>
         <div className='flex flex-col w-full p-4'>
@@ -93,7 +93,7 @@ const Navbar = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
