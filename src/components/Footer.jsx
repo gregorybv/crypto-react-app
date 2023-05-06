030f686abf5bc8b640fa43357339dc6a96befa7e
Gregory <gregory_berezin@outlook.com>
2023-05-06 10:31:47 +0300

    new edits

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -41,7 +41,7 @@ const Footer = () => {
               <p className='text-center md:text-right'>Sign up for crypto news</p>
               <div className='py-4'>
                 <form>
-                  <input className='bg-primary border border-input p-2 mr-2 w-full shadow-xl rounded-2xl md:w-auto' type='email' placeholder='Enter your email' />
+                  <input className='bg-primary border border-input p-2 mr-2 w-full shadow-xl rounded-2xl md:w-auto outline-none' type='email' placeholder='Enter your email' />
                   <button className='bg-button text-btnText px-4 p-2 w-full rounded-2xl shadow-xl hover:shadow-2xl md:w-auto my-2'>Sign up</button>
                 </form>
               </div>
